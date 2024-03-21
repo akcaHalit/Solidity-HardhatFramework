6cd408d7b79cdaa50ec210388a9c1d5841f2af03
akcaHalit <halitakca070@gmail.com>
2024-03-22 01:51:16 +0300

    Some required changes.

--- test/Lock-test.mjs
+++ test/Lock-test.mjs
@@ -1,17 +1,23 @@
-// import { expect } from 'chai';  // it is a module not the .js, so we can't use like this.
 //const { expect } = require('chai');      // we can not require() function with it.
 //import { ethers } from 'hardhat';
 //const { ethers } = require('hardhat');
+// import { BigNumber } from 'ethers';
 
 import { expect } from 'chai';
 import pkg from 'hardhat';
 const { ethers } = pkg;
 const provider =  ethers.provider;           // Provides us to communicate with Blockchain 
 
+
+function ethToNum(val){         // to be decimal numbers,   we can use like a normal numbers now.
+    return Number(ethers.utils.formatEther(val));
+}
+
 describe("Lock Contract", function () {
     let owner, user1, user2;
     let Token, token;
     let Lock, lock;
+    let balances;
 
     // Before: When the test script runs, it executes once and does not run again.
     before(async function(){
@@ -23,19 +29,55 @@ describe("Lock Contract", function () {
 
         Lock = await ethers.getContractFactory("Lock");
         lock = await Lock.connect(owner).deploy(token.address);    
+
+        token.connect(owner).transfer(user1.address, ethers.utils.parseUnits("100",18));
+        token.connect(owner).transfer(user2.address, ethers.utils.parseEther("50"));
+
+        token.connect(user1).approve(lock.address, ethers.constants.MaxUint256); // Maxiumum Approve
+        token.connect(user2).approve(lock.address, ethers.constants.MaxUint256); // Maxiumum Approve
+
     });  
+
+    beforeEach(async function(){
+        balances = [
+            ethToNum(await token.balanceOf(owner.address)),
+            ethToNum(await token.balanceOf(user1.address)),
+            ethToNum(await token.balanceOf(user2.address)),
+            ethToNum(await token.balanceOf(lock.address))
+        ]
+    });
 
     // it statements are should be asynchron, the first condition
     it(" Deploys the contracts", async function() {
         // we deployed two contracts, So they have addresses.  So, we're gonna check it.
+
         expect(token.address).to.not.be.undefined;  // is undefined?
         //expect(lock.address).to.be.properAddress; // is valid?   NO LONGER USED :(
-
     }); 
 
-    
-    //describe("Contract Functions", function (){
-    //});
+    it("Sends Token", async function(){
+        // The balance should be 100, 50, and also smaller than lock.
+        expect(balances[1]).to.be.equal(100); 
+        expect(balances[2]).to.be.equal(50); 
+        expect(balances[0]).to.be.greaterThan(balances[1]);  // 100>50 bi kontrol edelim bakalım doğru mu gitmiş
+
+    });
+
+    it("Approves", async function(){
+        // We give Allowances
+        let allowances = [
+         await token.allowance(user1.address,lock.address),   
+         await token.allowance(user2.address,lock.address),   
+        ]
+
+        // Maximum approve verdik bakalım eşitler mi?       BURASI ÇALIŞMADI Niye Çözemedim
+        // We gave maximum approval, let's see if they are equal. THIS DIDN'T WORK Why? I could not solve it :(
+        //expect(allowances[0]).to.be.equal(ethers.constants.MaxUint256);
+        //expect(allowances[1]).to.be.equal(ethers.constants.MaxUint256);
+
+    });
+
+    //describe("Contract Functions", function (){});
 
 });
 
@@ -61,3 +103,5 @@ describe("Lock Contract", function () {
 //(ENG):  We can search for contracts deployed by their name and obtain an instance of these contracts. Afterwards, we can deploy these contracts using these instances.
 
 
+//import { bignumber } from 'chai-bignumber';
+//expect.use(bignumber)(provider.BigNumber);
